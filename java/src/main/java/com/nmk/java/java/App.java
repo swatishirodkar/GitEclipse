86338b317dd561ab012652a35bbe8fcb12e93b7d
swatishirodkar <swati.shirodkar@gmail.com>
2021-01-25 17:35:36 -0800

    Update App.java

--- java/src/main/java/com/nmk/java/java/App.java
+++ java/src/main/java/com/nmk/java/java/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World welcome to java!" );
-        System.out.println("You are in github for testing");
+        System.out.println("You are in github for testing done with testing");
                           
     }
 }
