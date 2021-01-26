e44782b58eaf745b0da04af2ace2edabb8fff302
Swati Shirodkar <swati.shirodkar@gmail.com>
2021-01-25 16:37:50 -0800

    Helloworld

---
+++ java/src/main/java/com/nmk/java/java/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.nmk.java.java;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Java ::::");
+	}
+
+}
