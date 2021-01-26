1d4673449965f60e6444cfa63cd5860093203a76
Swati Shirodkar <swati.shirodkar@gmail.com>
2021-01-26 11:50:12 -0800

    Indian

--- java/src/main/java/com/nmk/java/java/App.java
+++ java/src/main/java/com/nmk/java/java/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World welcome to java commited first test!" );
-        System.out.println("Happy Republic Day to alll the Indians firstly and lastly Indian");
+        System.out.println("Indians firstly and lastly Indian");
                           
     }
 }
