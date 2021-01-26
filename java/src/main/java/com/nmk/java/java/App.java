37e1566dd656b11f1a74437514f0e44f175c248e
Swati Shirodkar <swati.shirodkar@gmail.com>
2021-01-26 11:45:58 -0800

    "Republic Day"

--- java/src/main/java/com/nmk/java/java/App.java
+++ java/src/main/java/com/nmk/java/java/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World welcome to java commited first test!" );
-        System.out.println("You are in github for testing done with testing");
+        System.out.println("Happy Republic Day to alll the Indians firstly and lastly Indian");
                           
     }
 }
