d2ad36dbcd94b5ffa41228f5c3b54e3b5073d547
Swati Shirodkar <swati.shirodkar@gmail.com>
2021-01-25 16:32:29 -0800

    eclipse edition done by swati

--- Githubtesting/src/HelloWorld.java
+++ Githubtesting/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is the first github Project in eclipse");
+		System.out.println("This is the first github Project in eclipse  done by swati");
 		
 	}
 
